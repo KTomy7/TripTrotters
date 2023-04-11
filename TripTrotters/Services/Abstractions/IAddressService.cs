30fddaa9206672220c3ee69fb81f224fe707ea21
Ielciu Cristian <ielciu.cristian01@gmail.com>
2023-04-11 17:06:30 +0300

    Address Service

---
+++ TripTrotters/Services/Abstractions/IAddressService.cs
@@ -0,0 +1,15 @@
+﻿using TripTrotters.Models;
+
+namespace TripTrotters.Services.Abstractions
+{
+    public interface IAddressService
+    {
+        Task<IEnumerable<Address>> GetAll();
+        Task<Address> GetByIdAsync(int id);
+
+        bool Add(Address offer);
+        bool Update(Address offer);
+        bool Delete(Address offer);
+        bool Save();
+    }
+}
