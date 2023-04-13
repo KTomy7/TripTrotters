41a0d88e27675c2d60ded02711e7c5a929d03002
Ielciu Cristian <ielciu.cristian01@gmail.com>
2023-04-13 16:35:11 +0300

    Gata Offer CRUD

--- TripTrotters/Controllers/OfferController.cs
+++ TripTrotters/Controllers/OfferController.cs
@@ -11,11 +11,12 @@ namespace TripTrotters.Controllers
     {
 
         private readonly IOfferService _offerService;
-        private readonly IApartmentService _apartmentService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public OfferController(IOfferService offerService)
+        public OfferController(IOfferService offerService, IHttpContextAccessor httpContextAccessor)
         {
             _offerService = offerService;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<IActionResult> Index()
         {
@@ -31,26 +32,27 @@ namespace TripTrotters.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var curUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            var offerViewModel = new OfferViewModel { AgentId = int.Parse(curUserId) };
+            return View(offerViewModel);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(CreateOfferViewModel offerVM)
+        public async Task<IActionResult> Create(OfferViewModel offerViewModel)
         {
             if (!ModelState.IsValid)
             {
-                return View(offerVM);
+                return View(offerViewModel);
             }
 
             Offer offer = new Offer
             {
-                Title = offerVM.Title,
-                Description = offerVM.Description,
-                StartDate = offerVM.StartDate,
-                EndDate = offerVM.EndDate,
-                //TREBE LUAT USERU CURENT
-                AgentId = 3,
-                ApartmentId = offerVM.ApartmentId,
+                Title = offerViewModel.Title,
+                Description = offerViewModel.Description,
+                StartDate = offerViewModel.StartDate,
+                EndDate = offerViewModel.EndDate,
+                AgentId = offerViewModel.AgentId,
+                ApartmentId = offerViewModel.ApartmentId,
 
             };
 
@@ -58,5 +60,72 @@ namespace TripTrotters.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Offer offer = await _offerService.GetByIdAsync(id);
+            if(offer == null)
+            {
+                return View("Error");
+            }
+            var offerViewModel = new OfferViewModel
+            {
+                Title = offer.Title,
+                Description = offer.Description,
+                StartDate = offer.StartDate,
+                EndDate = offer.EndDate,
+                ApartmentId=offer.ApartmentId,
+            };
+
+            return View(offerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, OfferViewModel offerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit offer");
+                return View("Edit", offerViewModel);
+            }
+
+            Offer offer = await _offerService.GetByIdAsync(id);
+            if (offer == null)
+            {
+                return View("Error");
+            }
+               
+            offer.Title = offerViewModel.Title;
+            offer.Description = offerViewModel.Description;
+            offer.StartDate = offerViewModel.StartDate; 
+            offer.EndDate = offerViewModel.EndDate;
+            offer.ApartmentId = offerViewModel.ApartmentId;
+
+            _offerService.Update(offer);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+         public async Task<IActionResult> Delete(int id)
+        {
+            var offerDetails = await _offerService.GetByIdAsync(id);
+            if (offerDetails == null) return View("Error");
+            return View(offerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteOffer(int id)
+        {
+            var offerDetails = await _offerService.GetByIdAsync(id);
+            
+            if (offerDetails == null) return View("Error");
+
+            _offerService.Delete(offerDetails);
+            return RedirectToAction("Index");
+
+        }
     }
+
 }
