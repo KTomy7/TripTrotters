885abf90a809af9d52cc7e15b6d2f06441389c24
KTomy7 <kisstomi7.kt@gmail.com>
2023-05-16 17:47:28 +0300

    Added new field to AspNetUsers table

---
+++ TripTrotters/Migrations/20230516144254_AddNewFieldToAspNetUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripTrotters.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFieldToAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+        }
+    }
+}
