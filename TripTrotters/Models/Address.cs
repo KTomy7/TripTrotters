dd51cfae604edf980f20c5c28d98ea6b1a978350
KTomy7 <kisstomi7.kt@gmail.com>
2023-05-20 21:04:03 +0300

    CleanUp

--- TripTrotters/Models/Address.cs
+++ TripTrotters/Models/Address.cs
@@ -3,16 +3,10 @@
     public class Address
     {
         public int Id { get; set; }
-
         public string Country { get; set; }
-        
         public string City { get; set; }
-
         public string Street {get; set; }
-
         public int StreetNumber { get; set; }
-
         public Apartment Apartment { get; set; }
-
     }
 }
